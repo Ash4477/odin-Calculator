74c4cde3f5ab433aaceae8bea1848b3234a9c5c4
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-05-09 19:47:06 +0500

    update divide() and operate() to fix division bug | add clearScreen() method | add events to operator btns

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function multiply(num1,num2) {
 function divide(num1,num2) {
     if (num2 === 0) {
         alert("😵‍💫");
-        return;
+        return Infinity;
     }
     return num1 / num2;
 }
@@ -21,7 +21,7 @@ function operate(first_number,operator,second_number){
         
         case "-" : return substract(first_number,second_number);
 
-        case "/" : return divide(first_number,second_number);
+        case "÷" : return divide(first_number,second_number);
 
         case "x" : return multiply(first_number, second_number);
 
@@ -29,12 +29,18 @@ function operate(first_number,operator,second_number){
     }
 }
 
+// error for negative first number
 function getEquationValues(equationString) {
     let operator = equationString.split("").find(item => {
-       return (item == "+" || item == "-" || item == "/" || item == "x" || item == "%");
+       return (item == "+" || item == "-" || item == "÷" || item == "x" || item == "%");
     })
     console.log(operator);
     return [...equationString.split(operator), operator];
+}
+
+function clearScreen() {
+    document.querySelector("#screen").textContent = "";
+    eqnCheck = [false,false,false];
 }
 
 let eqnCheck = [false,false,false]; // for checking what values (num1, num2, or operator) haven been input in the screen
@@ -47,13 +53,50 @@ const equalBtn = document.querySelector("#equal-btn");
 numberBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         const screen = document.querySelector("#screen");
-        if (screen.textContent == "")
+        if (screen.textContent == ""){
             eqnCheck[0] = true;
+        }
         
         else {
-            if (eqnCheck[1] == true)
+            if (eqnCheck[1] == true){
                 eqnCheck[2] = true;
+            }
         }
+        screen.textContent += btn.textContent;
+    });
+});
+
+operatorBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const screen = document.querySelector("#screen");
+        // fix later
+        if (screen.textContent == "") {
+            if (btn.textContent == "-"){
+                eqnCheck[0] = true;
+                screen.textContent = "-";
+            }
+            return;
+        }
+
+        else if (eqnCheck[1] == true && eqnCheck[2] == false){
+            screen.textContent = screen.textContent.slice(0,-1);
+        }
+
+        else if (eqnCheck[2] == true) {
+            eqnCheck[2] = false;
+            let num1;
+            let num2;
+            let opr;
+
+            [num1,num2,opr] = getEquationValues(screen.textContent);
+            const result = operate((Number)(num1), opr, (Number)(num2));
+            if (result === Infinity) {
+                clearScreen();
+                return;
+            }
+            screen.textContent = result;
+        }
+        eqnCheck[1] = true;
         screen.textContent += btn.textContent;
     });
 });
