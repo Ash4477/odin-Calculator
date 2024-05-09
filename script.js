dbb71e355854a49b63367c667b417351825db92e
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-05-09 18:55:33 +0500

    fix operate method + add getEqnValues method

--- script.js
+++ script.js
@@ -23,9 +23,21 @@ function operate(first_number,operator,second_number){
 
         case "/" : return divide(first_number,second_number);
 
-        case "*" : return multiply(first_number, second_number);
+        case "x" : return multiply(first_number, second_number);
 
         default: alert("Invalid Operation");
     }
 }
 
+function getEquationValues(equationString) {
+    let operator = equationString.split("").find(item => {
+       return (item == "+" || item == "-" || item == "/" || item == "x" || item == "%");
+    })
+    console.log(operator);
+    return [...equationString.split(operator), operator];
+}
+
+let number1;
+let number2;
+let operator;
+let eqnCheck = [false,false,false]; // for checking what values (num1, num2, or operator) haven been input in the screen
